e0dff6d2d41619f5bbad2c7caecd6b1b481d4a83
Horler Raphael <hraphaelh@gmail.com>
2021-07-30 11:04:58 +0200

    [add] rangefilter sur annonce (kilometrage, anneecirculation, prix)

--- src/Entity/Annonce.php
+++ src/Entity/Annonce.php
@@ -2,15 +2,18 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\AnnonceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
 
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass=AnnonceRepository::class)
+ * @ApiFilter(RangeFilter::class, properties={"kilometrage", "anneeCirculation", "prix"})
  */
 class Annonce
 {
